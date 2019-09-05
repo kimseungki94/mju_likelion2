c81f52cbc6ad94f36c25dad9d7e71ec9857a2ae3
kimseungki94 <skkim0832@naver.com>
2019-09-05 23:55:48 +0900

    game complate

--- Week1/game.js
+++ Week1/game.js
@@ -16,12 +16,17 @@ let choosenum3;
 let select1;
 let select2;
 let select3;
+let selectall = [];
+let text = "";
+let face = "";
+let winnings = 0; //승리 횟수
+let randomall = [];
 //자신이 원하는 베팅금액 입력
 function myFunction() {
     document.getElementById("bettingmoneybtn").innerHTML = "선택확인";
-    let showbetting = document.getElementById("betMoney")
-    showbetting.innerHTML = document.getElementById("betting").value;
-    funds = showbetting.innerHTML;
+    let showbetting = document.getElementById("betMoney") 
+    showbetting.innerHTML = document.getElementById("betting").value;  //funds가 버튼클릭시 보이게끔 만듬
+    funds = showbetting.innerHTML;  //funds값을 여기서 초기화 시킴
 
 }
 function selectfunction1() {
@@ -57,27 +62,21 @@ function outputfunction3() {
     let choose3num = document.getElementById("betting3").value;
     choosenum3 = choose3num;
 }
-function sample() {
-    document.getElementById("sample").innerHTML = `되라 -> ${funds}`;
-}
-
-function showround() {
-    document.getElementById("round").innerHTML = `${round}`;
-}
 
 function start() {
-    if (funds > 1 && funds < 100) {
+     console.log(funds)
+    if (funds > 1) {
         round++;
 
         console.log(`round ${round}:`);
         console.log(`\t starting funds: ${funds}원`);
         let bets = {
-            crown: 0,
-            anchor: 0,
-            heart: 0,
-            spade: 0,
-            club: 0,
-            diamond: 0
+            크라운: 0,
+            앵커: 0,
+            하트: 0,
+            스페이드: 0,
+            클럽: 0,
+            다이아몬드: 0
         };
         //라운드 횟수
         document.querySelector("#result .round").innerHTML = "라운드 :" + round;
@@ -86,7 +85,6 @@ function start() {
 
         console.log(`totalBet ${totalBet}:`);
         //판돈 나누기
-
         let remaining = funds;
         bets[select1] = bets[select1] + parseInt(choosenum1);
         bets[select2] = bets[select2] + parseInt(choosenum2);
@@ -105,102 +103,55 @@ function start() {
         marblevalue1.innerHTML = "주사위에 나온 문양 : " + marblevalue1.value;
 
         marblevalue2.value = randFace();
-        marblevalue2.innerHTML = marblevalue2.value;
+        marblevalue2.innerHTML = " " + marblevalue2.value;
 
         marblevalue3.value = randFace();
-        marblevalue3.innerHTML = marblevalue3.value;
+        marblevalue3.innerHTML = " " + marblevalue3.value;
 
-        let winnings = 0; //승리 횟수
-        let earn = 0; //수익금
-        //돈딴거 계산     
-        if (String(select1) === String(marblevalue1.value)) {
-            let comparebet = document.getElementById("comparebet");
-            let comparebet123 = document.getElementById("comparebet123");
-            comparebet.innerHTML = "응땃어~"
-            comparebet123.innerHTML = ""
-            winnings = winnings +1;
-            earn = earn + parseInt(choosenum1);
-        } 
-        else if (String(select1) === String(marblevalue2.value)) {
-            let comparebet = document.getElementById("comparebet");
-            let comparebet123 = document.getElementById("comparebet123");
-            comparebet.innerHTML = "응땃어~";
-            comparebet123.innerHTML = "";
-            winnings = winnings +1;
-            earn = earn + parseInt(choosenum1);
-        } 
-        else if (String(select1) === String(marblevalue3.value)) {
-            let comparebet = document.getElementById("comparebet");
-            let comparebet123 = document.getElementById("comparebet123");
-            comparebet.innerHTML = "응땃어~"
-            comparebet123.innerHTML = ""
-            winnings = winnings +1;
-            earn = earn + parseInt(choosenum1);
-        } 
-        if (String(select2) === String(marblevalue1.value)) {
-            let comparebet = document.getElementById("comparebet");
-            let comparebet123 = document.getElementById("comparebet123");
-            comparebet.innerHTML = "응땃어~"
-            comparebet123.innerHTML = ""
-            winnings = winnings +1;
-            earn = earn + parseInt(choosenum2);
-        }
-        else if (String(select2) === String(marblevalue2.value)) {
-            let comparebet = document.getElementById("comparebet");
-            let comparebet123 = document.getElementById("comparebet123");
-            comparebet.innerHTML = "응땃어~"
-            comparebet123.innerHTML = ""
-            winnings = winnings +1;
-            earn = earn + parseInt(choosenum2);
-        }
-        else if (String(select2) === String(marblevalue3.value)) {
-            let comparebet = document.getElementById("comparebet");
-            let comparebet123 = document.getElementById("comparebet123");
-            comparebet.innerHTML = "응땃어~"
-            comparebet123.innerHTML = ""
-            winnings = winnings +1;
-            earn = earn + parseInt(choosenum2);
-        }
-        if (String(select3) === String(marblevalue1.value)) {
-            let comparebet = document.getElementById("comparebet");
-            let comparebet123 = document.getElementById("comparebet123");
-            comparebet.innerHTML = "응땃어~"
-            comparebet123.innerHTML = ""
-            winnings = winnings +1;
-            earn = earn + parseInt(choosenum3);
-        }
-        else if (String(select3) === String(marblevalue2.value)) {
-            let comparebet = document.getElementById("comparebet");
-            let comparebet123 = document.getElementById("comparebet123");
-            comparebet.innerHTML = "응땃어~"
-            comparebet123.innerHTML = ""
-            winnings = winnings +1;
-            earn = earn + parseInt(choosenum3);
-        }
-        else if (String(select3) === String(marblevalue3.value)) {
-            let comparebet = document.getElementById("comparebet");
-            let comparebet123 = document.getElementById("comparebet123");
-            comparebet.innerHTML = "응땃어~"
-            comparebet123.innerHTML = ""
-            winnings = winnings +1;
-            earn = earn + parseInt(choosenum3);
-        }
-        if(winnings == 0){
-            comparebet.innerHTML = "인생...."
-            comparebet123.innerHTML = ""
-        } 
-        let wingame = document.getElementById("winningnum");
-        wingame.innerHTML = "이긴횟수 : " +winnings;
+        //돈딴거 계산    
+        selectall.push(select1);
+        selectall.push(select2);
+        selectall.push(select3);
+        randomall[0]= marblevalue1.value;
+        randomall[1]= marblevalue2.value;
+        randomall[2]= marblevalue3.value;
 
-        document.getElementById("earning").innerHTML = "수익금 : " +earn;
+        if (text == "") {
+            for (i = 0; i < selectall.length; i++) {
+                text += selectall[i] + " ";
+            }
+            
+            document.getElementById("test").innerHTML = "사용자가선택한 문양 : " + text;
+        }
 
 
 
-        for (let die = 0; die < hand.length; die++) {
-            let face = hand[die];
-            if (bets[face] > 0) winnings = winnings + bets[face];
+        for (die = 0; die < 3; die++) {
+            face = randomall[die];
+            console.log("주사위 재대로 들어갓니?"+randomall[die]);
+            if (bets[face] > 0) {         
+                winnings = winnings + bets[face];
+                console.log("위닝 재대로 들어갓니?"+winnings);
+                console.log("bet 재대로 들어갓니?"+bets[face]);
+            }
         }
-        funds = funds + winnings;
-        console.log(`\twinngings: ${winnings}`);
+        //document.getElementById("stop").innerHTML = bets[face];
+        funds = remaining + winnings;
+        document.getElementById("result1").innerHTML = "딴돈 : " + winnings;
+        document.getElementById("result2").innerHTML = "현재있는돈 : " + funds;
+        document.getElementById("image1").innerHTML = "크라운 : " +  bets['크라운'];
+        document.getElementById("image2").innerHTML = "앵커 : " +  bets['앵커'];
+        document.getElementById("image3").innerHTML = "하트 : " +  bets['하트'];
+        document.getElementById("image4").innerHTML = "클럽 : " +  bets['클럽'];
+        document.getElementById("image5").innerHTML = "스페이드 : " +  bets['스페이드'];
+        document.getElementById("image6").innerHTML = "다이아몬드 : " +  bets['다이아몬드'];
+        bets['크라운']=0;
+        bets['앵커']=0;
+        bets['하트']=0;
+        bets['클럽']=0;
+        bets['스페이드']=0;
+        bets['다이아몬드']=0;
+        winnings=0;
+
     }
 }
