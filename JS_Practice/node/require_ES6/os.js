9ac379ba974c8dd13959423fa5b3623799878907
kimseungki94 <skkim0832@naver.com>
2019-10-07 07:42:44 +0900

    show OS information

---
+++ JS_Practice/node/require_ES6/os.js
@@ -0,0 +1,7 @@
+const os = require('os');
+
+console.log('------------ 운영체제 정보 -------------'); 
+console.log('os.arch():',os.arch());
+console.log('os.platform():',os.platform);
+console.log('os.uptime():',os.uptime());
+console.log('os.hostname():',os.hostname());
