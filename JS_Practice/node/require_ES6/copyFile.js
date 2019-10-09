1ddfa06fbaf9ff2fd44e632cb2474a935e338397
kimseungki94 <skkim0832@naver.com>
2019-10-09 17:55:18 +0900

    file in/output & express & json

---
+++ JS_Practice/node/require_ES6/copyFile.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+fs.copyFile('readme4.txt','writeme4.txt',(error)=>{
+    if(error){
+        throw console.error(error);
+    }
+    console.log('복사 완료');
+});
+
---
+++ JS_Practice/node/require_ES6/fsPromises.js
@@ -0,0 +1,3 @@
+const fsPromises = require('fs').promises;
+
+//최근부턴 promises도 지원함!
